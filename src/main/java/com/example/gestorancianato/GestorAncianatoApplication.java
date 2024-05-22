4d3c3473ff11e7bcf8dfa1ec02f1c171fd96d018
JuanP10 <ramirezgutierrez32@gmail.com>
2024-05-21 21:20:26 -0500

    Inicializando proyecti

---
+++ src/main/java/com/example/gestorancianato/GestorAncianatoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestorancianato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorAncianatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorAncianatoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestorancianato/GestorAncianatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestorancianato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorAncianatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
